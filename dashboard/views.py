ee90c490cd9acf580cec9dc63ae898ecef981e70
Schmaexxi <langknecht@stud.uni-heidelberg.de>
2019-05-29 20:46:56 +0200

    screenshot working instance before database remodelling

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,8 @@ from dashboard.models import Votings, Individual_votings
 
 
 def index(request):
+    voting_behavior = Individual_votings.objects.filter(voting__SPD__contains=[{'name': 'Annen'}])
+    print(voting_behavior)
     return render(request, 'dashboard/index.html', {})
 
 
@@ -14,10 +16,14 @@ def list(request):
 
 def detail(request, voting_id):
     voting_parties = Individual_votings.objects.filter(voting_id=voting_id)[0]
-    print(voting_parties.voting)
     # only works if first value is not an empty list
     b = [key for key in voting_parties.voting[[k for k in voting_parties.voting.keys()][0]][0].keys()]
-    specific_voting = Votings.objects.filter(voting_id=voting_id)[0]
+    all_votings = Votings.objects.all()
+    specific_voting = all_votings.filter(voting_id=voting_id)[0]
+    votes = [int(n) for n in specific_voting.json['votes'].values()]
     return render(request, 'dashboard/detail.html', {'all_factions': voting_parties,
+                                                     'all_votings': all_votings,
                                                      "specific_voting": specific_voting,
+                                                     "vote_labels": [lbl for lbl in specific_voting.json['votes'].keys()],
+                                                     "votes": votes,
                                                      "attributes": b})
